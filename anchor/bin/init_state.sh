931fd858c2f40e86baacda05980e0a811934c9b0
mimno <mimno@cornell.edu>
2015-08-03 16:04:26 -0400

    support for different numbers of topics

--- anchor/bin/init_state.sh
+++ anchor/bin/init_state.sh
@@ -2,15 +2,22 @@
 
 INSTANCES=$1
 OUTDIR=$2
+TOPICS=${3:-30}
 
 mkdir -p $OUTDIR
 
-bin/mallet train-topics --input $INSTANCES --output-state $OUTDIR/initial_state.gz --num-iterations 0
+INITIAL_STATE=$OUTDIR/initial_state-$TOPICS.gz
+PRUNED_INSTANCES=$OUTDIR/instances.pruned
+ANCHORS=$OUTDIR/anchors-$TOPICS.txt
+TOPIC_WORDS=$OUTDIR/topic_words-$TOPICS.txt
+OUTPUT_STATE=$OUTDIR/anchor_state-$TOPICS.gz
 
-bin/mallet prune --input $INSTANCES --output $OUTDIR/instances.pruned --prune-count 10
+bin/mallet train-topics --input $INSTANCES --output-state $INITIAL_STATE --num-iterations 0 --num-topics $TOPICS
 
-bin/mallet run cc.mallet.classify.tui.Vectors2Info --input $OUTDIR/instances.pruned --print-features > $OUTDIR/vocab.txt
+bin/mallet prune --input $INSTANCES --output $PRUNED_INSTANCES --prune-count 10
 
-bin/anchor train-anchor --input $OUTDIR/instances.pruned --anchors-file $OUTDIR/anchors.txt --num-topics 30 --topics-file $OUTDIR/topic_words.txt --min-docs 100 --num-random-projections 2000 --projection-density 0.1
+bin/mallet run cc.mallet.classify.tui.Vectors2Info --input $PRUNED_INSTANCES --print-features > $OUTDIR/vocab.txt
 
-gzcat $OUTDIR/initial_state.gz | perl -w bin/modify-state.pl $OUTDIR/vocab.txt $OUTDIR/topic_words.txt | gzip > $OUTDIR/anchor_state.gz
+bin/anchor train-anchor --input $PRUNED_INSTANCES --anchors-file $ANCHORS --num-topics $TOPICS --topics-file $TOPIC_WORDS --min-docs 100 --num-random-projections 2000 --projection-density 0.1
+
+gzcat $INITIAL_STATE | perl -w bin/modify-state.pl $OUTDIR/vocab.txt $TOPIC_WORDS | gzip > $OUTDIR/anchor_state-$TOPICS.gz
