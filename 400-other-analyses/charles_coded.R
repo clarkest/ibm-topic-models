551f3c276686a11fbcf82fbb4c05e8bde5c3a23d
Clark Bernier <clarkest@gmail.com>
2016-09-07 10:14:47 -0400

    supervised learning setup

--- 400-other-analyses/charles_coded.R
+++ 400-other-analyses/charles_coded.R
@@ -1,14 +1,10 @@
-charles.coded <- read.csv("/Users/clarkbernier/Dropbox/Semantic analysis/Topic model 2015/Data/VJ-WJ corpus with CH type additions.csv")
-charles.coded <- select(charles.coded, -name)
-write.csv(charles.coded, file="/Users/clarkbernier/Dropbox/Semantic analysis/Topic model 2015/Data/VJ-WJ corpus with CH type additions.csv")
 charles.coded <- read.csv("place_docs_here/VJ-WJ with CH types recovered (edited).csv")
 
-charles.coded <- read.csv("place_docs_here/charles_coded_docs.csv", stringsAsFactors = F)
 names(charles.coded)
 table(charles.coded$TYPE)
 
 table(charles.coded$continent, charles.coded$jam)
-table(threaded.docs$continent, threaded.docs$jam)
+table(th.doc.topics$continent, th.doc.topics$jam)
 
 charles.coded$TYPE[charles.coded$TYPE == "collab"] <- "Collab"
 charles.coded$TYPE[charles.coded$TYPE == "contract"] <- "Contract"
@@ -16,43 +12,34 @@ charles.coded$TYPE[charles.coded$TYPE == "trad"] <- "Trad"
 charles.coded$TYPE[charles.coded$TYPE == "xCn"] <- "0"
 charles.coded$TYPE[charles.coded$TYPE == "xTr"] <- "0"
 charles.coded$TYPE[charles.coded$TYPE == "dup"] <- "0"
+charles.coded$TYPE[charles.coded$TYPE == "Contract"] <- "0"
+charles.coded$TYPE[charles.coded$TYPE == "0"] <- "neither"
 charles.coded$TYPE <- factor(charles.coded$TYPE)
 table(charles.coded$TYPE)
 sum(is.null(charles.coded$parent))
 sum(is.null(threaded.docs$parent))
 
-coded.docs <- threaded.docs %>%
+coded.docs <- th.doc.topics %>%
   left_join(select(charles.coded, id, TYPE), by="id")  
 coded.docs$TYPE[is.na(coded.docs$TYPE)] <- ""
+table(coded.docs$TYPE)
 coded.docs %>% select(-ancestors) %>%
-  write.csv(file="~/threaded_docs_coded.csv")
+  write.csv(file="place_docs_here/threaded_docs_coded.csv")
+# coded.docs <- read.csv("place_docs_here/threaded_docs_coded.csv", stringsAsFactors=F)
 
-
-
-
-
-#######
-# data for Charles
-#######
-
-threaded.docs %>% select(-ancestors) %>%
-  write.csv(file="~/orig_threaded_docs.csv")
-
-
-id.set <- c("ffdb040327.efbf4e62.break_down_the_silos", "ffda9923eb.23f00123.break_down_the_silos")
-View(filter(threaded.docs, root.id %in% id.set))
+mgr.types <- table(coded.docs$new.mgr, coded.docs$TYPE)[,-1] 
+mgr.types
+round(mgr.types / rowSums(mgr.types),3)
 
 
 
 ########
 # train a supervised model on the codes
 ########
-mgr.types <- table(coded.docs$new.mgr, coded.docs$TYPE)[,-1] 
-mgr.types
-round(mgr.types / rowSums(mgr.types),3)
-
-
+# coded.docs <- read.csv("place_docs_here/threaded_docs_coded.csv", stringsAsFactors=F)
 library(quanteda)
+library(dplyr)
+coded.docs[coded.docs$TYPE == "0", "TYPE"] <- "neither"
 coded.docs$was.coded <- coded.docs$TYPE !=""
 corpus <- corpus(coded.docs$text, 
                  docnames=coded.docs$id, 
@@ -60,8 +47,59 @@ corpus <- corpus(coded.docs$text,
 stops <- as.character(read.table("200-topic-models/en.txt")$V1)
 corp.features <- dfm(corpus, ignoredFeatures=stops, stem=F)
 
-sub.corpus <- subset(corpus, coded.docs$was.coded)
+sub.corpus <- subset(corpus, coded.docs$was.coded & coded.docs$TYPE != "contract")
 sub.features <- dfm(sub.corpus, ignoredFeatures=stops, stem=F)
+sub.df <- as.data.frame(as.wfm(sub.features))
+
+
+library(doParallel)
+library(parallel)
+library(caret)
+sub.dtm.outcome <- cbind(sub.df, y = factor(sub.corpus$documents$TYPE))
+
+
+
+ctrl <- trainControl(method = "repeatedcv",
+                     repeats = 5, 
+                     number = 10)
+
+registerDoParallel(24, cores=24)
+fit.svm <- train(y ~ ., 
+                 data = sub.dtm.outcome, 
+                 method = 'svmLinear',
+                 trControl = ctrl)
+fit.rf <- train(y ~ ., 
+                 data = sub.dtm.outcome, 
+                 method = 'rf',
+                 trControl = ctrl,
+                verbose=T)
+rf.mid.grid <- expand.grid(mtry=c(100,150,250,1000,3000))
+fit.rf.mid <- train(y ~ ., 
+                data = sub.dtm.outcome, 
+                method = 'rf',
+                trControl = ctrl,
+                tuneGrid = rf.mid.grid,
+                verbose=T)
+fit.rngr <- train(y ~ ., 
+                data = sub.dtm.outcome, 
+                method = 'ranger',
+                trControl = ctrl)
+fit.knn <- train(y ~ ., 
+                 data = sub.dtm.outcome, 
+                 method = 'knn',
+                 trControl = ctrl)
+fit.dp <- train(y ~ ., 
+                 data = sub.dtm.outcome, 
+                 method = 'deepboost',
+                 trControl = ctrl)
+
+
+stopImplicitCluster()
+fit.svm
+
+fit.rf <- train(y ~ ., data = sub.dtm.outcome, method = 'rf')
+
+
 
 library(textir)
 type.val <- data.frame(type=as.numeric(sub.corpus$documents$TYPE=="Collab"))
@@ -77,6 +115,24 @@ coef(mnir.fit)[2, order(coefs[2, ])[1:10]]
 # most collaborative words (higher "type")
 coef(mnir.fit)[2, -order(coefs[2, ])[1:10]]
 
+
+
+
+
+
+
+
+
+#######
+# data for Charles
+#######
+
+threaded.docs %>% select(-ancestors) %>%
+  write.csv(file="~/orig_threaded_docs.csv")
+
+
+id.set <- c("ffdb040327.efbf4e62.break_down_the_silos", "ffda9923eb.23f00123.break_down_the_silos")
+View(filter(threaded.docs, root.id %in% id.set))
 
 
 
